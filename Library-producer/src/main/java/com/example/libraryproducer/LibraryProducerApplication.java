c66ce8ac6b6f61ab415ee5f1cd2851c7cb8d8177
MikBac <mbachorz1997@gmail.com>
2022-12-19 01:04:50 +0100

    init producer

---
+++ Library-producer/src/main/java/com/example/libraryproducer/LibraryProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraryproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryProducerApplication.class, args);
+	}
+
+}
---
+++ Library-producer/src/test/java/com/example/libraryproducer/LibraryProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
